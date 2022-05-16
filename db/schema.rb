b594a74dc7c0311df1f211a672c2a66e05a3cca7
minkhati <meetup.ror@gmail.com>
2022-05-16 23:10:23 +0000

    switch to postgresql database in production and setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_16_200707) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_16_200707) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
